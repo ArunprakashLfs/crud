7ac5bed691e26c65c87138323c679d6a6fadbe3c
kesavan <arunprakash7871@gmail.com>
2023-03-21 03:42:18 -0400

    gett input

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+//Getting input from users
+let inputValue = document.getElementById("input-text");
+let inputTime = document.getElementById("input-value");
+
+let submitEl = document.getElementById("submit");
+let outputEl = document.getElementById("output");
