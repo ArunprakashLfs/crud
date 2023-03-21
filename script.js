f525cf19cf2b212e713191348920ba2446948cf8
kesavan <arunprakash7871@gmail.com>
2023-03-21 03:49:37 -0400

    add form in js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //Getting input from users
 let inputValue = document.getElementById("input-text");
 let inputTime = document.getElementById("input-value");
+let formEl = document.getElementById("form")
 console.log(inputValue);
 console.log(inputTime);
 
@@ -9,6 +10,6 @@ let submitEl = document.getElementById("submit");
 let outputEl = document.getElementById("output");
 
 //add Event listners
-form.addEventListener('submit', function (e) {
+formEl.addEventListener('submit', function (e) {
     e.preventDefault();
 })
