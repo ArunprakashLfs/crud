fc14778c01b095ea3716962eadc591eb00726ac7
kesavan <arunprakash7871@gmail.com>
2023-03-21 04:04:34 -0400

    add javascript and changed button properties

--- script.js
+++ script.js
@@ -3,13 +3,19 @@
 let inputValue = document.getElementById("input-text");
 let inputTime = document.getElementById("input-value");
 let formEl = document.getElementById("form")
-console.log(inputValue);
-console.log(inputTime);
+// console.log(inputValue);
+// console.log(inputTime);
 
-let submitEl = document.getElementById("submit");
+// let submitEl = document.getElementById("submit");
+// console.log(submitEl);
 let outputEl = document.getElementById("output");
 
 //add Event listners
 formEl.addEventListener('submit', function (e) {
     e.preventDefault();
+    let input = inputValue.value;
+    let time = inputTime.value;
+    console.log(input);
+    console.log(time);
+    // console.log(formEl);
 })
