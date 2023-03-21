64c7c780f263fee56da5bfc8e25ed61931c83b68
kesavan <arunprakash7871@gmail.com>
2023-03-21 03:46:40 -0400

    add input

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 //Getting input from users
 let inputValue = document.getElementById("input-text");
 let inputTime = document.getElementById("input-value");
+console.log(inputValue);
+console.log(inputTime);
 
 let submitEl = document.getElementById("submit");
 let outputEl = document.getElementById("output");
+
+//add Event listners
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+})
