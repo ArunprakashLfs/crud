189d3c21e0d5ff0386613a869e172bdcbc769f6b
kesavan <arunprakash7871@gmail.com>
2023-03-21 04:27:01 -0400

    add array in js

--- script.js
+++ script.js
@@ -6,16 +6,23 @@ let formEl = document.getElementById("form")
 // console.log(inputValue);
 // console.log(inputTime);
 
+//creating empty array for store data
+let todoList = [];
+
 // let submitEl = document.getElementById("submit");
 // console.log(submitEl);
 let outputEl = document.getElementById("output");
 
 //add Event listners
-formEl.addEventListener('submit', function (e) {
+formEl.addEventListener("submit", function (e) {
     e.preventDefault();
     let input = inputValue.value;
     let time = inputTime.value;
     console.log(input);
     console.log(time);
+    todoList = [input, time];
+    console.log(todoList);
+
+
     // console.log(formEl);
 })
