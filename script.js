c95651451eab55f1aa225c6d5667e8d6c2a811f3
kesavan <arunprakash7871@gmail.com>
2023-03-24 00:41:39 -0400

    add input element

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict'
+//getting input elements
+let inputEl = document.getElementById("input-text");
+let timeEl = document.getElementById("input-value");
+let submitEl = document.getElementById("submit");
+let outputEl = document.getElementById("output");
