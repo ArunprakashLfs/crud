8f4d0463098a6fab5cf0bf963442b2408e1e4f36
kesavan <arunprakash7871@gmail.com>
2023-03-24 00:45:14 -0400

    add event listners

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ let inputEl = document.getElementById("input-text");
 let timeEl = document.getElementById("input-value");
 let submitEl = document.getElementById("submit");
 let outputEl = document.getElementById("output");
+
+//functions
+
+
+//add eventlistner
+submitEl.addEventListener("click", function (e) {
+    e.preventDefault();
+    let input = inputEl.value;
+    let time = timeEl.value;
+    console.log(input, time);
+    
+})
